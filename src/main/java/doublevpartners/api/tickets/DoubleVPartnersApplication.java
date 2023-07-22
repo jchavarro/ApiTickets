57a91462ae5594d2cbcc737e9f8690134e7d2267
Juan Camilo Chavarro <j.chavarro@utp.edu.co>
2023-07-22 10:32:07 -0500

    config inicial

---
+++ src/main/java/doublevpartners/api/tickets/DoubleVPartnersApplication.java
@@ -0,0 +1,13 @@
+package doublevpartners.api.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleVPartnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleVPartnersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/doublevpartners/api/tickets/DoubleVPartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package doublevpartners.api.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleVPartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
